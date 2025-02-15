82bbf67d70fc6891e4bdd27c64cf641e489fa8a5
mohd2548 <125661601+mohd2548@users.noreply.github.com>
2025-02-15 23:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b,c): # conflict issue
     return a + b + c
 
-def subtraction(a,b,c): # only for understanding conflict issue
-    return a - b - c
+def subtraction(a,b,e): # only for understanding conflict issue
+    return a - b - e
 
 def division(a,b):
     return a/b
